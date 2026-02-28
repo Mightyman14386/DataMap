3d243cae12e93aa0e749b81c7a621eebe2914a99
Will Wentink <wwentink@wisc.edu>
2026-02-28 17:39:06 -0600

    tests working for apis

--- datamap/src/env.js
+++ datamap/src/env.js
@@ -11,15 +11,12 @@ export const env = createEnv({
 			process.env.NODE_ENV === "production"
 				? z.string()
 				: z.string().optional(),
-		AUTH_GOOGLE_ID: z.string(),
-		AUTH_GOOGLE_SECRET: z.string(),
-		DATABASE_URL: z.string().url(),
 		GOOGLE_CLIENT_ID: z.string(),
 		GOOGLE_CLIENT_SECRET: z.string(),
+		DATABASE_URL: z.string().url(),
 		OPENAI_API_KEY: z.string().optional(),
 		ANTHROPIC_API_KEY: z.string().optional(),
 		HIBP_API_KEY: z.string().optional(),
-		DATABASE_URL: z.string().optional(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("development"),
@@ -40,14 +37,12 @@ export const env = createEnv({
 	 */
 	runtimeEnv: {
 		AUTH_SECRET: process.env.AUTH_SECRET,
-		AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-		AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
 		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
 		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+		DATABASE_URL: process.env.DATABASE_URL,
 		OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 		ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
 		HIBP_API_KEY: process.env.HIBP_API_KEY,
-		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
 	},
 	/**
