5e8a947ed7af4cdfc246dd52fffd60827e43ebfc
Ryan Sinnreich <47066669+ryan-sinnreich@users.noreply.github.com>
2026-02-28 17:07:12 -0600

    Frontend

--- datamap/src/app/layout.tsx
+++ datamap/src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang="en">
-			<body className={geist.className}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body className={geist.className} suppressHydrationWarning>{children}</body>
 		</html>
 	);
 }
