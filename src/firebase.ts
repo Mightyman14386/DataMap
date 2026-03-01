019ba676e418c1c50db11a4d0e58f527520919f2
germilla <germilla@wisc.edu>
2026-02-28 19:01:43 -0600

    Firebase working

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Log this to your terminal temporarily to see if it's actually working
+console.log("Connecting to Project:", process.env.REACT_APP_FIREBASE_PROJECT_ID);
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID as string,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID as string,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || ""
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export the Firestore instance
 export const db = getFirestore(app);
