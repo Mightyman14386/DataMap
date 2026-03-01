54a4dcd24f5695b40aada38b2a97b996c5e0eab1
ragi68 <raghav.shankar.iyer@gmail.com>
2026-02-28 23:28:46 -0600

    React website packages

--- .next/types/app/Frontend/authorize/page.ts
+++ .next/types/app/Frontend/authorize/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\willw\Hackathons\DataMap\src\app\Frontend\authorize\page.tsx
+// File: C:\Prototypes\DataMap\src\app\Frontend\authorize\page.tsx
 import * as entry from '../../../../../src/app/Frontend/authorize/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
